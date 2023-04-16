64182a5d12c2cf5590eb28c4c0a584698d6b0028
Michael Haberler <git@mah.priv.at>
2023-04-16 12:09:14 +0200

    make i2cscan work on both m5 and plain esp32

--- src/i2cscan.cpp
+++ src/i2cscan.cpp
@@ -1,5 +1,5 @@
 
-#ifdef M5STACK
+#ifdef M5_I2CSCAN
 
 #include <M5Unified.h>
 
@@ -7,54 +7,50 @@ void i2cScan(void) {
   Serial.printf("\ninternal I2C devices: ('Wire'):\n");
   bool scan[128];
   M5.In_I2C.scanID(scan);
-  for (int i = 8; i < 0x78; ++i) {
+  for (int i = 8; i < 0x7f; ++i) {
     if (scan[i]) {
       Serial.printf("intern: 0x%x\n", i);
     }
   }
+#ifdef SECONDARY_I2C_PORT
   Serial.printf("\nexternal I2C (red port) devices: ('Wire1'):\n");
   M5.Ex_I2C.scanID(scan);
-  for (int i = 8; i < 0x78; ++i) {
+  for (int i = 8; i < 0x7f; ++i) {
     if (scan[i]) {
       Serial.printf("extern: 0x%x\n", i);
     }
   }
+#endif
   Serial.println();
 }
-#endif
 
-#ifdef WROVER_KIT
+#else
+
 #include <Arduino.h>
 #include <Wire.h>
 
-void i2cScan(void) {
-  byte error, address;
-  int nDevices;
-  Serial.println("Scanning...");
-  nDevices = 0;
-  for (address = 8; address < 0x78; address++) {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-    if (error == 0) {
-      Serial.print("I2C device found at address 0x");
-      if (address < 16)
-        Serial.print("0");
-      Serial.print(address, HEX);
-      Serial.println("  !");
-      nDevices++;
-    } else if (error == 4) {
-      Serial.print("Unknown error at address 0x");
-      if (address < 16)
-        Serial.print("0");
-      Serial.println(address, HEX);
+void printI2CBusScan(TwoWire &theWire, const char *tag) {
+  theWire.begin();
+  for (uint8_t addr = 0x8; addr <= 0x7f; addr++) {
+    theWire.beginTransmission(addr);
+    if (theWire.endTransmission() == 0) {
+      Serial.printf("%s0x%x\n", tag, addr);
     }
   }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
+  Serial.println();
+}
+
+void i2cScan(void) {
+  Serial.println("");
+  Serial.println("");
+  Serial.printf("Default port (Wire) I2C scan\n");
+  printI2CBusScan(DEFAULT_I2C_PORT, "");
+
+#if defined(SECONDARY_I2C_PORT)
+  Serial.printf("Secondary port (Wire1) I2C scan\n");
+  printI2CBusScan(SECONDARY_I2C_PORT, "";
+#endif
+  Serial.println("");
+  Serial.println("");
 }
 #endif
